1be41124eb1d26030dd2179936d8c72fc4d0097b
Irvin SAmboy <irvinsamboy@gmail.com>
2025-03-11 11:13:49 -0400

    Creating tasks table

--- app/db/schemas.py
+++ app/db/schemas.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import column, Integer, String
+from sqlalchemy import column, Integer, String, DateTime
 
 Base = declarative_base()
 
@@ -15,6 +15,11 @@ class taskType(Base):
     __tableNAme__ = "taskType"
 
     idTask = column(Integer, primary_key=True)
-    taskNAme = column(String, nulleable=False)
+    typeNAme = column(String, nulleable=False)
     typeIdentifier = column(String, nulleable=False)
 
+class tasks(Base):
+    idTask = column(Integer, primary_key=True)
+    taskName = column(String, nulleable=False)
+    taskDescription = column(String, nulleable=False)
+    taskExpiration = column(DateTime, nulleable=True)
